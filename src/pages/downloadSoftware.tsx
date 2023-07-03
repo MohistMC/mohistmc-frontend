9ca3d2ab9a25a5cf865d43f7ffcab23bf684abbf
shawiizz <shaweereso@gmail.com>
2023-07-03 16:45:37 +0200

    ✨ Customize build search filters

--- src/pages/downloadSoftware.tsx
+++ src/pages/downloadSoftware.tsx
@@ -18,6 +18,18 @@ export default function downloadSoftware() {
     const [viewedBuildPages, setViewedBuildPages] = useState<Build[][]>([])
     const [currentPage, setCurrentPage] = useState<number>(0)
     const [selectedVersion, setSelectedVersion] = useState<string | undefined>()
+    const [noResult, setNoResult] = useState<boolean>(false)
+    const [filters, setFilters] = useState<{
+        buildNumber: boolean,
+        buildName: boolean,
+        buildMd5: boolean
+        buildDate: boolean
+    }>({
+        buildNumber: true,
+        buildName: true,
+        buildMd5: true,
+        buildDate: true
+    })
 
     useEffect(() => {
         if (router.isReady) {
@@ -74,6 +86,7 @@ export default function downloadSoftware() {
         // If no search query, reset the pages
         if (search.length === 0) {
             setViewedBuildPages(originalBuildPages)
+            setNoResult(false)
             return
         }
 
@@ -81,9 +94,21 @@ export default function downloadSoftware() {
         const modifiedBuildPages: Build[] = []
 
         for (const page of originalBuildPages) {
-            const filteredPage = page.filter((build) => String(build.number).toLowerCase().includes(search.toLowerCase()) || build.fileMd5.toLowerCase().includes(search.toLowerCase()) || build.fileName?.toLowerCase().includes(search.toLowerCase()))
+            const filteredPage = page.filter((build) =>
+                (filters.buildNumber && String(build.number).toLowerCase().includes(search.toLowerCase())) ||
+                (filters.buildMd5 && build.fileMd5.toLowerCase().includes(search.toLowerCase())) ||
+                (filters.buildName && build.fileName?.toLowerCase().includes(search.toLowerCase())) ||
+                (filters.buildDate && new Date(build.createdAt).toDateString().toLowerCase().includes(search.toLowerCase())
+            ))
             modifiedBuildPages.push(...filteredPage)
         }
+
+        // Check if there is no result
+        if (modifiedBuildPages.length === 0)
+            setNoResult(true)
+
+        // TODO: Si il n'y a pas de résultat, mettre un texte plutot que le loader
+        // TODO: Quand on séléctionne une version faut fermer le menu
 
         // Split the builds into pages
         const modifiedPages: Build[][] = []
@@ -91,6 +116,25 @@ export default function downloadSoftware() {
             modifiedPages.push(modifiedBuildPages.slice(i, i + perPage))
 
         setViewedBuildPages(modifiedPages)
+    }
+
+    const handleFilter = (event: ChangeEvent<HTMLInputElement>) => {
+        const filter = event.target.name
+
+        switch (filter) {
+            case 'buildNumber':
+                setFilters({...filters, buildNumber: !filters.buildNumber})
+                break
+            case 'buildName':
+                setFilters({...filters, buildName: !filters.buildName})
+                break
+            case 'buildMd5':
+                setFilters({...filters, buildMd5: !filters.buildMd5})
+                break
+            case 'buildDate':
+                setFilters({...filters, buildDate: !filters.buildDate})
+                break
+        }
     }
 
     return (
@@ -103,21 +147,75 @@ export default function downloadSoftware() {
 
             <div
                 className="relative shadow-md dark:shadow-md dark:bg-dark-50 bg-white sm:rounded-lg mt-10 p-5">
-                <div className={`flex md:justify-between md:gap-0 gap-2 justify-center items-center pb-4 flex-wrap`}>
-                    <div className="bg-white dark:bg-dark-50">
-                        <label htmlFor="table-search" className="sr-only">Search for builds</label>
-                        <div className="relative mt-1">
-                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                     fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd"
-                                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                          clipRule="evenodd"></path>
-                                </svg>
+                <div className={`flex md:justify-between gap-2 justify-center items-center pb-4 flex-wrap`}>
+                    <div className={`flex gap-2 items-center justify-center`}>
+                        <div className="bg-white dark:bg-dark-50">
+                            <label htmlFor="table-search" className="sr-only">Search for builds</label>
+                            <div className="relative mt-1">
+                                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                    <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                         fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fillRule="evenodd"
+                                              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                              clipRule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <input type="text" id="table-search" onChange={handleSearch}
+                                       className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-dark-200 dark:border-dark-400 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       placeholder="Search for builds"></input>
                             </div>
-                            <input type="text" id="table-search" onChange={handleSearch}
-                                   className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-dark-200 dark:border-dark-400 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                   placeholder="Search for builds"></input>
+                        </div>
+                        <button id="dropdownBgHoverButton" data-dropdown-toggle="dropdownBgHover"
+                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 mt-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                type="button">Filter by <svg className="w-4 h-4 ml-2" aria-hidden="true"
+                                                             fill="none" stroke="currentColor"
+                                                             viewBox="0 0 24 24"
+                                                             xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M19 9l-7 7-7-7"></path>
+                        </svg></button>
+
+                        <div id="dropdownBgHover"
+                             className="z-10 hidden w-48 bg-white rounded-lg shadow dark:bg-gray-700">
+                            <ul className="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200"
+                                aria-labelledby="dropdownBgHoverButton">
+                                <li>
+                                    <div
+                                        className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                                        <input defaultChecked={true} id="checkbox-item-4" type="checkbox" value="" name={`buildNumber`} onChange={handleFilter}
+                                               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"></input>
+                                        <label htmlFor="checkbox-item-4"
+                                               className="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">Build number</label>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div
+                                        className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                                        <input defaultChecked={true} id="checkbox-item-5" type="checkbox" value="" name={`buildName`} onChange={handleFilter}
+                                               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"></input>
+                                        <label htmlFor="checkbox-item-5"
+                                               className="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">Build name</label>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div
+                                        className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                                        <input defaultChecked={true} id="checkbox-item-6" type="checkbox" value="" name={`buildMd5`} onChange={handleFilter}
+                                               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"></input>
+                                        <label htmlFor="checkbox-item-6"
+                                               className="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">MD5 Checksum</label>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div
+                                        className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                                        <input defaultChecked={true} id="checkbox-item-7" type="checkbox" value="" name={`buildDate`} onChange={handleFilter}
+                                               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"></input>
+                                        <label htmlFor="checkbox-item-7"
+                                               className="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">Build date</label>
+                                    </div>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                     <VersionSelectorElement selectedVersion={selectedVersion} setSelectedVersion={setSelectedVersion}
@@ -138,7 +236,7 @@ export default function downloadSoftware() {
                         </th>
                         <th scope="col" className="px-6 py-3 hidden md:table-cell">
                             <div className="flex items-center">
-                                Built on
+                                Build date
                                 <Link href="#">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-3 ml-1"
                                          aria-hidden="true"
@@ -184,9 +282,14 @@ export default function downloadSoftware() {
                     }
                     </tbody>
                 </table>
-                {viewedBuildPages.length === 0 &&
+                {viewedBuildPages.length === 0 && !noResult &&
                     <div className="flex justify-center mt-4">
                         <LoadingParagraph/>
+                    </div>
+                }
+                {noResult &&
+                    <div className="flex justify-center mt-4">
+                        <p className="text-gray-500 dark:text-gray-400">No builds found</p>
                     </div>
                 }
                 <NavigationTableElement buildPages={viewedBuildPages} currentPage={currentPage}
