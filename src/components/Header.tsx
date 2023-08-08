8e256eaa031651cb4908bc59eef252bda92bef59
shawiizz <shaweereso@gmail.com>
2023-08-08 18:34:03 +0200

    🐛 Fix API text not blue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -145,7 +145,7 @@ export default function Header() {
                         </li>
                         <li>
                             <Link href="/mohistmc-api"
-                                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-dark-200 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                  className={`block py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white md:dark:hover:bg-transparent md:dark:bg-transparent dark:border-gray-700 md:bg-transparent ${pageName === 'mohistmc-api' ? `md:text-blue-700 md:dark:text-blue-500 bg-blue-700 text-white` : 'hover:bg-gray-100 dark:hover:bg-dark-200'}`}>
                                 {strings['button.api']}
                             </Link>
                         </li>
