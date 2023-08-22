e972260400fb5081e25aac079b5f478b82062941
shawiizz <shaweereso@gmail.com>
2023-08-22 14:54:53 +0200

    🐛 Fix dev env url

--- src/components/SwaggerComponent.tsx
+++ src/components/SwaggerComponent.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import dynamic from 'next/dynamic'
 import "swagger-ui-react/swagger-ui.css"
+import {getAPIEndpoint} from "@/util/Environment";
 
 const SwaggerUI = dynamic(() => import('swagger-ui-react'), {ssr: false});
 
 export default function SwaggerComponent({onComplete}: { onComplete: () => void }) {
-    return <SwaggerUI url="https://mohistmc.com/api/v2/docs/json" docExpansion="list" onComplete={onComplete}/>
+    return <SwaggerUI url={`${getAPIEndpoint()}/docs/json`} docExpansion="list" onComplete={onComplete}/>
 }
