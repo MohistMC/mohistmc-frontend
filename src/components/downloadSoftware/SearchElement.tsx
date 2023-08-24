53410574e041d208d3e7f419ed5bdac10766b67f
shawiizz <shaweereso@gmail.com>
2023-08-24 23:30:40 +0200

    🐛 Fix exact match not selected when specified in URL params

--- src/components/downloadSoftware/SearchElement.tsx
+++ src/components/downloadSoftware/SearchElement.tsx
@@ -278,7 +278,7 @@ export default function SearchElement({
                 </div>
             </Dropdown>
             <div className="flex items-center gap-2">
-                <Checkbox id="exactMatch" onChange={handleExactMatch}/>
+                <Checkbox id="exactMatch" checked={exactMatchChecked} onChange={handleExactMatch}/>
                 <Label className="flex" htmlFor="exactMatch">
                     <p>
                         {strings['downloadSoftware.search.exactMatch']}
