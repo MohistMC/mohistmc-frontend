e431b48bd4347efcf4f9809a426643c870b8ec38
黑嫖233 <heipiao233@outlook.com>
2024-04-03 21:43:42 +0800

    Fix loading (#6)

--- src/util/Environment.ts
+++ src/util/Environment.ts
@@ -4,5 +4,5 @@
  * @param version - The version of the API
  */
 export const getAPIEndpoint = (version: string = 'v2') => {
-    return process.env.NODE_ENV === "development" ? `http://localhost:2024/api/${version}` : `https://mohistmc.com/api/${version}`;
+    return process.env.NODE_ENV === "development" ? `http://localhost:2024/api/${version}` : `https://www.mohistmc.com/api/${version}`;
 }
